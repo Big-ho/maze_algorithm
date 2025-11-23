e1e6975fd5f9627d7ba496e6d0487e0be0e4413c
daeho-haruto <kimdaeho2004@naver.com>
2025-11-23 20:00:43 +0900

    추가: BMP 파일 리더 및 2차원 bgr 구조체 배열로 변환

---
+++ include/maze_c/image_reader.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#pragma pack(push, 1)
+
+typedef struct {
+  unsigned short bf_type;
+  unsigned int bf_size;
+  unsigned short bf_reserved1;
+  unsigned short bf_reserved2;
+  unsigned int bf_offbits;
+} bitmap_file_header_t;
+
+typedef struct {
+  unsigned int bi_size;
+  int bi_width;
+  int bi_height;
+  unsigned short bi_planes;
+  unsigned short bi_bit_count;
+  unsigned int bi_compression;
+  unsigned int bi_size_image;
+  int bi_xpels_per_meter;
+  int bi_ypels_per_meter;
+  unsigned int bi_clr_used;
+  unsigned int bi_clr_important;
+} bitmap_info_header_t;
+
+typedef struct {
+  unsigned char blue;
+  unsigned char green;
+  unsigned char red;
+  unsigned char reserved;
+} rgb_quad_t;
+
+#pragma pack(pop)
+
+enum {
+  MAX_MAZE_SIZE = 1000,
+};
+
+rgb_quad_t **load_bmp(const char *filename, int *height, int *width);
+void free_bmp(rgb_quad_t **img, int height);
+rgb_quad_t **create_bmp(int height, int width);
