cded5a1d74da52506f5d040ba547fc2d80d1578e
daeho-haruto <kimdaeho2004@naver.com>
2025-12-01 19:19:56 +0900

    추가: 실시간 키 입력 간단한 설명 추가

--- src/utils/terminal.c
+++ src/utils/terminal.c
@@ -29,27 +29,30 @@ void sleep_ms(int ms) { Sleep(ms); }
 #include <termios.h>
 #include <unistd.h>
 
-// 실시간 키입력을 위한 termios 라이브러리 함수들
-// TODO: 자세한건 나중에 설명
-
 struct termios oldt;
 
+/*
+tcsetattr 터미널 속성 설정
+STDIN_FILENO - 키보드 표준입력
+TCSANOW - 즉시 변경
+oldt 백업 설정
+*/
 void reset_term() { tcsetattr(STDIN_FILENO, TCSANOW, &oldt); }
 void init_term() {
-  struct termios newt;
-  tcgetattr(STDIN_FILENO, &oldt);
-  newt = oldt;
-  newt.c_lflag &= ~(ICANON | ECHO);
-  newt.c_cc[VMIN] = 1;
-  newt.c_cc[VTIME] = 0;
-  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  struct termios newt;              // 새로운 설정
+  tcgetattr(STDIN_FILENO, &oldt);   // 기존 설정 가져오기
+  newt = oldt;                      // 기존 설정 넣기
+  newt.c_lflag &= ~(ICANON | ECHO); // 엔터 기다림, 입력 문자 출력 기능 끄기
+  newt.c_cc[VMIN] = 1;              // 1바이트가 들어오면 읽음
+  newt.c_cc[VTIME] = 0;             // 시간 제한없음
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt); // 설정
 }
 int kbhit() {
-  struct timeval tv = {0L, 0L};
-  fd_set fds;
-  FD_ZERO(&fds);
-  FD_SET(STDIN_FILENO, &fds);
-  return select(1, &fds, NULL, NULL, &tv);
+  struct timeval tv = {0L, 0L};            // 대기 시간 0
+  fd_set fds;                              // 파일 디스크립터
+  FD_ZERO(&fds);                           // 초기화
+  FD_SET(STDIN_FILENO, &fds);              // 키보드를 감시 대상
+  return select(1, &fds, NULL, NULL, &tv); // 입력 들어왔는지 감시
 }
 void sleep_ms(int ms) { usleep(ms * 1000); }
 
